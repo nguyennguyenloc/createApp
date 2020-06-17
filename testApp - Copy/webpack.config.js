30b6703cfbbc93ecb87e41b0e1ac701676ad57d2
nguyennguyenloc <rephoto209@outlook.com>
2020-06-17 17:34:11 +0700

    CreateApp

--- testApp - Copy/webpack.config.js
+++ testApp - Copy/webpack.config.js
@@ -18,7 +18,9 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015', 'react']
+                    // presets: ['es2015', 'react']
+                    presets: ['@babel/env', '@babel/react']
+
                 }
             }
         ]
